b1ae52a18843cfb69e86b0499a2dd9543fa68784
giusepped <giusdesan@gmail.com>
2016-12-20 11:48:47 +0000

    started writing tests for statement-mapper

--- spec/graph_mapper_spec.rb
+++ spec/graph_mapper_spec.rb
@@ -14,12 +14,17 @@ describe Grom::GraphMapper do
     end
   end
 
-  xdescribe '#statements_mapper' do
-    it 'should return a hash with the mapped predicates and the respective objects from a graph' do
-      arya = extended_class.statements_mapper(PEOPLE_GRAPH).select{ |o| o[:id] == '2' }.first
-      expect(arya[:forename]).to eq 'Arya'
-      surname_pattern = RDF::Query::Pattern.new(:subject, RDF::URI.new("#{DATA_URI_PREFIX}/schema/surname"), :object)
-      expect(arya[:graph].query(surname_pattern).first_object.to_s).to eq 'Stark'
+  describe '#statement_mapper' do
+    it 'should insert a hash with the id from a given statement in rdf format into the given hash' do
+      result = {}
+      extended_class.statement_mapper(ONE_STATEMENT_STUB, result)
+      expect(result["1"][:id]).to eq '1'
+    end
+
+    it 'should modify a given hash with the predicate and object from a given statement in ttl format' do
+      result = {}
+      extended_class.statement_mapper(ONE_STATEMENT_STUB, result)
+      expect(result).to eq 'bla'
     end
   end
 
