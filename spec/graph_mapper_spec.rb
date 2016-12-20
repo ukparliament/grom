ce175f462f5401d352d09ae6390d19dd4db9ccdd
giusepped <giusdesan@gmail.com>
2016-12-20 11:27:16 +0000

    finished testing through_split_graph

--- spec/graph_mapper_spec.rb
+++ spec/graph_mapper_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Grom::GraphMapper do
   let(:extended_class) { Class.new { extend Grom::GraphMapper } }
+  let(:through_split_graph_result) { extended_class.through_split_graph(BLANK_PARTY_MEMBERSHIP_TTL_BY_PARTY) }
 
   describe '#get_id' do
     it 'should return the id if given a uri' do
@@ -19,6 +20,28 @@ describe Grom::GraphMapper do
       expect(arya[:forename]).to eq 'Arya'
       surname_pattern = RDF::Query::Pattern.new(:subject, RDF::URI.new("#{DATA_URI_PREFIX}/schema/surname"), :object)
       expect(arya[:graph].query(surname_pattern).first_object.to_s).to eq 'Stark'
+    end
+  end
+
+  describe '#through_split_graph' do
+    it 'should return the associated hash containing the first party' do
+      expect(through_split_graph_result[:associated_class_hash]["23"][:partyName]).to eq 'Targaryens'
+    end
+
+    it 'should return the associated hash containing the second party' do
+      expect(through_split_graph_result[:associated_class_hash]["26"][:partyName]).to eq 'Dothrakis'
+    end
+
+    it 'should return the through class hash containing the first party membership associated with the first party' do
+      expect(through_split_graph_result[:through_class_hash]["_:node123"][:associated_object_id]).to eq '23'
+      expect(through_split_graph_result[:through_class_hash]["_:node123"][:partyMembershipStartDate]).to eq '1953-01-12'
+      expect(through_split_graph_result[:through_class_hash]["_:node123"][:partyMembershipEndDate]).to eq '1954-01-12'
+    end
+
+    it 'should return the through class hash containing the second party membership associated with the second party' do
+      expect(through_split_graph_result[:through_class_hash]["_:node124"][:associated_object_id]).to eq '26'
+      expect(through_split_graph_result[:through_class_hash]["_:node124"][:partyMembershipStartDate]).to eq '1954-01-12'
+      expect(through_split_graph_result[:through_class_hash]["_:node124"][:partyMembershipEndDate]).to eq '1955-03-11'
     end
   end
 
