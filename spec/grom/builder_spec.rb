d044c73dd83b3b96ef19763e76a61c198dac34b6
Callum Neve-Jones <c.nevejones@gmail.com>
2017-06-13 15:07:43 +0100

    [WESITE-1413] Return an array of objects when the they share an ID
    * Updated Grom::Node#populate to return an array of objects instead of the last object if they share a subject
    * Added additional specs
    * Bumped version number

--- spec/grom/builder_spec.rb
+++ spec/grom/builder_spec.rb
@@ -10,14 +10,27 @@ describe Grom::Builder do
   let(:connections_by_subject) { CONNECTIONS_BY_SUBJECT }
 
   describe '#build_objects_by_subject' do
-    subject {Grom::Builder.new(reader)}
-
     context 'data passed' do
+      subject {Grom::Builder.new(reader)}
+
       it 'creates all of the objects for each subject' do
         reader.instance_variable_set(:@statements_by_subject, statements_by_subject)
         reader.instance_variable_set(:@subjects_by_type, subjects_by_type)
         writer = subject.build_objects_by_subject
         expect(writer.instance_variable_get(:@objects).size).to eq(6)
+      end
+    end
+
+    context 'multiple objects' do
+      let(:multiple_object_reader) { Grom::Reader.new(StringIO.new(File.read('spec/fixtures/constituency.nt'))) }
+      subject {Grom::Builder.new(multiple_object_reader)}
+
+      it 'create array of objects' do
+        writer = subject.build_objects_by_subject
+        objects = writer.instance_variable_get(:@objects)
+
+        expect(objects.last.constituencyAreaExtent).to be_a(Array)
+        expect(objects.last.constituencyAreaExtent.size).to eq(2)
       end
     end
   end
@@ -44,5 +57,4 @@ describe Grom::Builder do
       end
     end
   end
-
 end
